15c45df578f73b10d706fe97a37e3c69c69c7ec9
Suchismit4 <Suchismit4@gmail.com>
2023-11-12 03:18:51 -0500

    Homepage backup (from merge error)

---
+++ backup-style.js
@@ -0,0 +1,74 @@
+import { StyleSheet} from 'react-native';
+
+
+// Styling for the main window that covers everything
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ecf0f1', // off-white color
+    },
+    widgetContainer: {
+        paddingLeft: 30,
+        paddingRight: 30,
+    },
+    sectionHeader: {
+        fontFamily: 'ClashDisplay',
+        fontSize: 20
+    },
+    sectionSubText: {
+        fontSize: 15,
+        marginTop: 7,
+        marginBottom: 30
+    },
+    widget: {
+        height: 170,
+        width: 330,
+        backgroundColor: "#D9D9D9",
+        borderRadius: 6,
+        overflow: 'hidden'
+    },
+    image: {
+        height: 170,
+        paddingVertical: 26,
+        paddingHorizontal: 35
+    },
+    textHeader: {
+        fontFamily: 'ClashDisplay',
+        color: "#fff",
+        fontSize: 21
+    },
+    textOneliner: {
+        fontSize: 16,
+        color: "#ecf0f1",
+        fontWeight: 600,
+        opacity: 0.9,
+        marginTop: 6
+    },
+    creatorContainer: {
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+    iconForMaker: {
+        width: 40,
+        height: 40,
+        borderRadius: 100,
+        marginTop: 10,
+        borderWidth: 4,
+        borderColor: '#881D1C'
+    },
+    whoMade: {
+        fontFamily: 'ClashDisplay',
+        fontSize: 16,
+        color: "#fff",
+        marginTop: 6,
+        marginLeft: 10
+    },
+    whoAll: {
+        fontSize: 13,
+        opacity: 0.8,
+        marginLeft: 10
+    }
+})
+
+export default styles
---
+++ backup.js
@@ -0,0 +1,114 @@
+import React, { useRef, useState, useEffect, useCallback } from "react";
+import { FlatList, SafeAreaView, Text, View, ScrollView, ImageBackground, TouchableOpacity, Image } from "react-native";
+import styles from "./style";
+import * as Font from "expo-font";
+import Navbar from "./components/major/navbar/main";
+import QuickActionBar from "./components/minor/quickCall/main";
+
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+  const [fontError, setFontError] = useState(false);
+
+  //
+  useEffect(() => {
+    (async () => {
+      try {
+        await Font.loadAsync({
+          ClashDisplay: require("./assets/fonts/ClashDisplay.ttf"),
+        });
+        setFontLoaded(true);
+      } catch (error) {
+        setFontError(true);
+        console.error("Error loading font", error);
+      }
+    })();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontLoaded || fontError) {
+      await console.log("Loaded font..");
+    }
+  }, [fontLoaded, fontError]);
+
+  if (!fontLoaded && !fontError) {
+    return null;
+  }
+
+  const renderProjects = ({ item, index }) => {
+    return (
+      <TouchableOpacity>
+        <View
+          style={[
+            styles.widget,
+            { marginLeft: index == 0 ? 30 : 0 },
+            { marginRight: index == projects.length - 1 ? 30 : 25 },
+          ]}
+        >
+          <ImageBackground source={{uri: 'https://i.imgur.com/qeGhWee.png'}} resizeMode="cover" style={styles.image}>
+            <Text style={styles.textHeader}>{item.name}</Text>
+            <Text style={styles.textOneliner}>{item.desc}</Text>
+            <View style={styles.creatorContainer}>
+              <Image style={styles.iconForMaker} source={{uri: 'https://i.imgur.com/UPgjPaH.png'}} />
+              <View>
+                <Text style={styles.whoMade}>{item.username}</Text>
+                <Text style={styles.whoAll}>and {item.teamSize} others...</Text>
+
+              </View>
+            </View>
+          </ImageBackground>
+        </View>
+      </TouchableOpacity>
+      
+    );
+  };
+
+  // You can add more projects
+  const projects = [{name: "Project #1", username: 'Stella Dey', teamSize: 17, desc: 'A scheduling app for college students.'}] 
+
+  return (
+    <View onLayout={onLayoutRootView} style={styles.container}>
+      <SafeAreaView>
+        <Navbar />
+        <ScrollView>
+          <View style={[styles.widgetContainer, { marginTop: 30 }]}>
+            {/* Section 1 */}
+            <View>
+              <Text style={styles.sectionHeader}>
+                Students are working on...
+              </Text>
+              <Text style={styles.sectionSubText}>
+                View what others are involved in.
+              </Text>
+            </View>
+          </View>
+          <FlatList
+            horizontal={true}
+            data={projects}
+            renderItem={renderProjects}
+            keyExtractor={(item) => item}
+            showsVerticalScrollIndicator={false}
+            showsHorizontalScrollIndicator={false}
+          />
+          <View style={[styles.widgetContainer, { marginTop: 30 }]}>
+            <QuickActionBar />
+            {/* Section 1 */}
+            <View style={{ marginTop: 35 }}>
+              <Text style={styles.sectionHeader}>Find research projects</Text>
+              <Text style={styles.sectionSubText}>
+                View what professors are working on.
+              </Text>
+            </View>
+          </View>
+          <FlatList
+            horizontal={true}
+            data={projects}
+            renderItem={renderProjects}
+            keyExtractor={(item) => item}
+            showsVerticalScrollIndicator={false}
+            showsHorizontalScrollIndicator={false}
+          />
+        </ScrollView>
+      </SafeAreaView>
+    </View>
+  );
+}
